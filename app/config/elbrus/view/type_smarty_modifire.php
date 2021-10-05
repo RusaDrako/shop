8405aadba37c7257602534345c55b6f74c65b862
Petukhov Leonid <rusadrako@yandex.ru>
2021-10-05 16:23:55 +0300

    Настройки модификаторов Smarty

--- app/config/elbrus/view/type_smarty_modifire.php
+++ app/config/elbrus/view/type_smarty_modifire.php
@@ -18,7 +18,9 @@ function smarty_modifier__registry() {
 	return [
 		'fio'            => __NAMESPACE__ .'\smarty_modifier__fio',
 		'phone'          => __NAMESPACE__ .'\smarty_modifier__phone_format',
+		'phone_a'        => __NAMESPACE__ .'\smarty_modifier__phone_a',
 		'phone_hidden'   => __NAMESPACE__ .'\smarty_modifier__phone_hidden',
+		'email_a'        => __NAMESPACE__ .'\smarty_modifier__email_a',
 		'email_hidden'   => __NAMESPACE__ .'\smarty_modifier__email_hidden',
 		'str_hidden'     => __NAMESPACE__ .'\smarty_modifier__str_hidden',
 		'tmp_dir'        => __NAMESPACE__ .'\smarty_modifier__template_dir',
@@ -47,10 +49,31 @@ function smarty_modifier__fio($value, $param = '%f %i %o') {
 
 
 /** Функция-модификатор для смарти: Телефон */
+function _phone_format($value) {
+	return preg_replace("/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/ui", '+$1 ($2) $3-$4-$5', $value);
+}
+
+
+
+
+
+/** Функция-модификатор для смарти: Телефон */
 function smarty_modifier__phone_format($value) {
 	$value = preg_replace('/[^0-9]/ui', '', $value);
 	$value = mb_substr($value, 0, 11);
-	return preg_replace("/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/ui", '+$1 ($2) $3-$4-$5', $value);
+	return _phone_format($value);
+}
+
+
+
+
+
+/** Функция-модификатор: Формат телефона */
+function smarty_modifier__phone_a($value) {
+	$value = preg_replace('/[^0-9]/ui', '', $value);
+	$value = mb_substr($value, 0, 11);
+	$phone = _phone_format($value);
+	return "<a href=\"tel:+{$value}\">{$phone}</a>";
 }
 
 
@@ -62,6 +85,15 @@ function smarty_modifier__phone_hidden($value) {
 	$value = preg_replace('/[^0-9]/ui', '', $value);
 	$value = mb_substr($value, 0, 11);
 	return '+' . mb_substr($value, 0, 1) . ' (' . mb_substr($value, 1, 3) . ') ***-**-' . mb_substr($value, -2);
+}
+
+
+
+
+
+/** Функция-модификатор: Скрытая строка */
+function smarty_modifier__email_a($value) {
+	return "<a href=\"mailto:$value\">{$value}</a>";
 }
 
 
